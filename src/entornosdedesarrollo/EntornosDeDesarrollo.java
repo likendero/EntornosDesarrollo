b6e8471cf667a57a99c479c620d5ef4c6bc941d3
likendero <likendero@likendero-lap2>
2017-11-10 09:30:50 +0100

    implementacion de jframe

--- src/entornosdedesarrollo/EntornosDeDesarrollo.java
+++ src/entornosdedesarrollo/EntornosDeDesarrollo.java
@@ -1,12 +1,12 @@
 
 package entornosdedesarrollo;
-
+    import javax.swing.*;
 /**
  *esta es la clase principal del proyecto creado para Entornos de Desarrollo
  * aqui se inicia el proyecto al ejecutar.
  * @author liken
  */
-public class EntornosDeDesarrollo {
+public class EntornosDeDesarrollo extends JFrame {
 
     /**
      * @param args the command line arguments
@@ -16,7 +16,13 @@ public class EntornosDeDesarrollo {
         Persona maria = new Persona(14,"maria","mujer");
         System.out.println(paco.saludo());
         System.out.println(paco.compareTo(maria));
+        JFrame patata= new JFrame("patata");
         
+        
+        patata.setSize(300, 300);
+        patata.add(new JTextField("hola que tal"));
+        patata.setVisible(true);
+        patata.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
